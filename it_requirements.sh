1e6c34908b6d65f345cd2441629acec15f31cde6
Usha Nemani <usha.nemani@jda.com>
2019-07-15 17:49:30 +0530

    numpy pandas test2

--- it_requirements.sh
+++ it_requirements.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 set -xeo pipefail
 
+echo "uninstalling numpy and pandas"
 
-if [[ "${KARTOTHEK_ARROW_VERSION}" = "0.XX.0" ]]; then
+pip uninstall -y numpy pandas
 
-   echo "uninstalling numpy and pandas"
+echo "installing Nightly build of numpy pandas"
 
-   pip uninstall -y numpy pandas
-
-   echo "installing Nightly build of numpy pandas"
-
-   PRE_WHEELS="https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com"
-   pip install --pre --no-deps --upgrade --timeout=60 -f $PRE_WHEELS numpy pandas
-
-
-else
-
-fi
+PRE_WHEELS="https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com"
+pip install --pre --no-deps --upgrade --timeout=60 -f $PRE_WHEELS numpy pandas
