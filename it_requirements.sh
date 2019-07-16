2733146a728827997dd6bd2b549977f012e18a91
Usha Nemani <usha.nemani@jda.com>
2019-07-16 11:38:54 +0530

    test with nightly build of Arrow

--- it_requirements.sh
+++ it_requirements.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-echo "uninstalling numpy and pandas"
+#echo "uninstalling numpy and pandas"
 
-pip uninstall -y numpy pandas
+#pip uninstall -y numpy pandas
 
 echo "installing Nightly build of numpy pandas"
 
 PRE_WHEELS="https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com"
+
 pip install --pre --no-deps --upgrade --timeout=60 --no-cache-dir -f $PRE_WHEELS numpy pandas
+
+echo "installing Nightly build of PY-ARROW"
+
+pip install --pre --no-deps --upgrade --timeout=60 --no-cache-dir -f "https://github.com/ursa-labs/crossbow/releases/download/latest" pyarrow
