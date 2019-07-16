c960f46ad3dfb1bcdda68886d461a7c22666ef2a
Usha Nemani <usha.nemani@jda.com>
2019-07-16 15:02:14 +0530

    pyarrow test8

--- it_requirements.sh
+++ it_requirements.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-echo "installing Nightly build of PY-ARROW"
+echo "Upgrading to  Nightly build of numpy pandas"
 
-pip install --pre --no-deps --upgrade --timeout=60 --no-cache-dir -f "https://github.com/ursa-labs/crossbow/releases/download/latest" pyarrow
+PRE_WHEELS="https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com"
 
+pip install --pre --no-deps --upgrade --timeout=180 --no-cache-dir -f $PRE_WHEELS numpy pandas
+
+echo "Upgrading to Nightly build of pyarrow"
+
+pip install --pre --no-deps --upgrade --timeout=180 --no-cache-dir -f "https://github.com/ursa-labs/crossbow/releases/download/latest" pyarrow
