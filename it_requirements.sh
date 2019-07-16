5380188a2aced1be59be170f4edec1baf0374891
Usha Nemani <usha.nemani@jda.com>
2019-07-16 14:56:06 +0530

    pyarrow test7

--- it_requirements.sh
+++ it_requirements.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
+echo "installing Nightly build of PY-ARROW"
 
-if [ "$${KARTOTHEK_ARROW_VERSION}" == "0.XX.0" ];
-then
-    echo "installing Nightly build of PY-ARROW"
+pip install --pre --no-deps --upgrade --timeout=60 --no-cache-dir -f "https://github.com/ursa-labs/crossbow/releases/download/latest" pyarrow
 
-    pip install --pre --no-deps --upgrade --timeout=60 --no-cache-dir -f "https://github.com/ursa-labs/crossbow/releases/download/latest" pyarrow
-
-    pytest --cov kartothek --cov-report xml --cov-report html
-
-fi
