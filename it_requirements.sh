4aaaf0964b30c20eee9ad05a895233382c76d420
Usha Nemani <usha.nemani@jda.com>
2019-07-16 10:46:24 +0530

    numpy pandas test5

--- it_requirements.sh
+++ it_requirements.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -xeo pipefail
 
 echo "uninstalling numpy and pandas"
 
@@ -8,4 +7,4 @@ pip uninstall -y numpy pandas
 echo "installing Nightly build of numpy pandas"
 
 PRE_WHEELS="https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com"
-pip install --pre --no-deps --upgrade --timeout=60 -f $PRE_WHEELS numpy pandas
+pip install --pre --no-deps --upgrade --timeout=60 --no-cache-dir -f $PRE_WHEELS numpy pandas
